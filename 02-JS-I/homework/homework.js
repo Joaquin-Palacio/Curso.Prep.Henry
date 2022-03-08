8c198af51c15ad060d5e0d739bee038f0e12a245
Joaquín-Palacio <joaquinpalacio2311@gmail.com>
2022-03-08 18:03:23 -0300

    Homework 2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -113,7 +113,7 @@ function esPar(num) {
   // Tu código:
   if (num % 2 === 0){
     return true;
-  } else{
+  } else {
     return false;
   }
 }
@@ -171,7 +171,7 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  if (numero > 0) {
+  if (numero > 0){
     return 'Es positivo';
   } else if (numero < 0){
     return 'Es negativo';
@@ -198,13 +198,14 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return 'Hola ' + nombre + '!';
+  return `Hola ${nombre}!`
+  //return 'Hola ' + nombre + '!'
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  return alto * ancho;
+  return alto * ancho ;
 }
 
 
@@ -236,12 +237,12 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if (letra > 1){
+  if (letra > 2){
     return 'Dato incorrecto';
-  } else if (letra === 'a' || letra === 'e' || letra === 'i'|| letra === 'o' || letra === 'u'){
+  } else if (letra === 'a' || letra === 'e'|| letra === 'i' || letra === 'o' || letra === 'u'){
     return 'Es vocal';
-  } else {
-     return 'Dato incorrecto'
+  } else{
+    return 'Dato incorrecto';
   }
 } 
 
