bcde67ccd9ca8673227fc43876fcf98a2c2d5fcf
Joaquín-Palacio <joaquinpalacio2311@gmail.com>
2022-03-09 10:14:14 -0300

    Homework 3

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -48,13 +48,13 @@ function saludo(idioma) {
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
   if (idioma === 'aleman'){
-    return 'Guten Tag!'
-  } else if(idioma === 'mandarin'){
-    return 'Ni Hao!'
+    return 'Guten Tag!';
+  } else if (idioma === 'mandarin'){
+    return 'Ni Hao!';
   } else if (idioma === 'ingles'){
-    return 'Hello!'
-  } else {
-    return 'Hola!'
+    return 'Hello!';
+  } else{
+    return 'Hola!';
   }
 }
 
@@ -66,7 +66,7 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
-  switch(color){
+  switch (color) {
     case 'blue':
       return 'This is blue';
     case 'red':
@@ -85,8 +85,10 @@ function esDiezOCinco(numero) {
   // De lo contrario, devuelve "false"
   // Tu código:
   if (numero === 10 || numero === 5){
-    return true;
-  } return false;
+   return true;
+  } else {
+   return false;
+  }
 }
 
 function estaEnRango(numero) {
@@ -95,7 +97,9 @@ function estaEnRango(numero) {
   // Tu código:
   if (numero < 50 && numero > 20){
     return true;
-  } return false;
+  } else {
+    return false;
+  }
 }
 
 function esEntero(numero) {
@@ -136,10 +140,10 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-  if (num1 < 0 || num2 < 0 || num3 < 0){
-    return 'Hay negativos';
-  } else if (num1 === 0 || num2 === 0 || num3 === 0){
+  if (num1 === 0 || num2 === 0 || num3 === 0){
     return 'Error';
+  } else if (num1 < 0 || num2 < 0 || num3 < 0){
+    return 'Hay negativos';
   } else if (num1 > num2 && num1 > num3 && num1 > 0){
     return 'Número 1 es mayor y positivo';
   } else if (num3 > num1 && num3 > num2){
@@ -155,15 +159,22 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  /*if (numero === 0 || numero === 1 || numero === 4) return false;
+  for (var i = 2; i < numero / 2; i++){
+    if (numero % i === 0){
+      return false;
+    }
+  } return true;*/
   if (numero < 2) return false;
   if (numero === 2) return true;
-  for (let i = 2; i < numero; i++){
+  for (var i = 2; i < numero; i++){
     if (numero % i === 0){
       return false;
     }
   }
   return true;
 }
+
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
@@ -180,9 +191,9 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  let tablaDel6 = [];
-  for (let i = 0; i < 11; i++){
-   tablaDel6.push( i * 6 );
+  var tablaDel6 = [];
+  for (var i = 0; i < 11; i++){
+    tablaDel6.push(i*6);
   }
   return tablaDel6;
 }
@@ -190,10 +201,10 @@ function tablaDelSeis(){
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  if (numero < 100 || numero > 999){
-    return false;
-  } else {
+  if (numero < 1000 && numero > 99){
     return true;
+  } else {
+    return false;
   }
 }
 
@@ -201,13 +212,12 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
-  let a = numero;
-  let i = 0;
+  var a = numero;
+  var i = 0;
   do {
-    i = i+1;
-    a = a+5;
-  }
-  while(i < 8);
+    i += 1
+    a += 5
+  } while (i < 8);
   return a;
 }
 
